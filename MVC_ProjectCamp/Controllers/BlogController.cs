951011c6e63227918941fc48607b0720c18f6c16
Ercan MUZIR <ercanmuzir@gmail.com>
2024-12-24 17:40:10 +0300

    Added PartialView and designed Homepage

---
+++ MVC_ProjectCamp/Controllers/BlogController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_ProjectCamp.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
