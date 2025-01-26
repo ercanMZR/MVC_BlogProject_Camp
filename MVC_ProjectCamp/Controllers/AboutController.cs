e8f07ac5f4e9e0f516a2fbf5ee53aab7b18e7885
Ercan MUZIR <ercanmuzir@gmail.com>
2025-01-26 15:53:58 +0300

    Add new services and update error page layout
    Added `IAboutService.cs` and `IContactService.cs` interfaces for About and Contact services.
    
    Implemented `AboutManager.cs` and `ContactManager.cs` to provide functionality for the About and Contact services using respective data access layers.
    
    Created `AboutController.cs` and `ContactController.cs` to handle actions for displaying About entities and managing contact form submissions.
    
    Added `Index.cshtml` for About and Contact sections to set up layouts and display content.
    
    Introduced `SocialMediaAbout.cshtml` for displaying social media accounts.

---
+++ MVC_ProjectCamp/Controllers/AboutController.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_ProjectCamp.Controllers
+{
+    public class AboutController : Controller
+    {
+        AboutManager abm = new AboutManager(new EfAboutRepository());
+        public IActionResult Index()
+        {
+            var values = abm.GetList();//AboutManager sınıfından türetilen abm nesnesi üzerinden GetList metotunu çağırır.
+            return View(values);
+        }
+
+        public PartialViewResult SocialMediaAbout()
+        {
+           
+            return PartialView();
+        }
+    }
+
+    
+}
---
+++ MVC_ProjectCamp/Controllers/ContactController.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_ProjectCamp.Controllers
+{
+    public class ContactController : Controller
+    {
+        ContactManager cm = new ContactManager(new EfContactRepository());
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Contact p)
+        {
+            p.ContactTime = DateTime.Parse(DateTime.Now.ToShortDateString());//güncel tarihi alır
+            p.ContactStatus = true;//mesajın durumu
+            cm.ContactAdd(p);//mesajı ekle
+            return RedirectToAction("Index", "Blog");//blog sayfasına yönlendir
+        }
+    }
+}
